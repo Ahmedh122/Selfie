f4f7c6bb054fed34286f155fc0955d753eba15e2
DhahriAhmed <ahmed.dhahri@studio.unibo.it>
2024-10-12 16:44:12 +0200

    aaaa

--- backend/connect.js
+++ backend/connect.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
-    process.exit(1); // Exit with failure
+    process.exit(1); 
   }
 };
 
